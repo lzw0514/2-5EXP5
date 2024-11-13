fa041bdfca99fe1bbeb783cf42345f8a562be6ad
lzw0514 <37220222203708@stu.xmu.edu.cn>
2024-11-13 17:33:17 +0800

    Initial commit

---
+++ src/main/java/cn/edu/xmu/javaee/productdemoaop/mapper/jpa/po/ProductEntity.java
@@ -0,0 +1,71 @@
+package cn.edu.xmu.javaee.productdemoaop.mapper.jpa.po;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "goods_product")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "goods_id")
+    private Long goodsId;
+
+    private String skuSn;
+
+    private String name;
+
+    private Long originalPrice;
+
+    private Long weight;
+
+    private String barcode;
+
+    private String unit;
+
+    private String originPlace;
+
+    private Long creatorId;
+
+    private String creatorName;
+
+    private Long modifierId;
+
+    private String modifierName;
+
+    private LocalDateTime gmtCreate;
+
+    private LocalDateTime gmtModified;
+
+    private Integer commissionRatio;
+
+    private Long freeThreshold;
+
+    private Byte status;
+
+
+    @OneToMany(mappedBy = "parentProduct", fetch = FetchType.LAZY)
+    private List<ProductEntity> otherProduct;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "goods_id", insertable = false, updatable = false)
+    private ProductEntity parentProduct;
+
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<OnSaleEntity> onSaleList;
+
+}
